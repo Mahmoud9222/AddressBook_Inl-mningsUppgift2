396ddba5521f06906e22854c0c902fe72263149a
Mahmoud <hamalawim@gmail.com>
2022-11-02 15:26:54 +0100

    Add project files.

---
+++ AddressBook_InlämningsUppgift2/models/ContactPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_InlämningsUppgift2.models
+{
+    public class ContactPerson
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;  
+        public string StreetName { get; set; } = null!; 
+        public string PostalCode { get; set; } = null!; 
+        public string City { get; set; } = null!;
+
+        public string FullName => $"{FirstName} {LastName}";
+
+    }
+}
